092be7c183808b68953684dc360733d71757cf36
Pablo Clemente <pclementep@gmail.com>
2021-03-22 12:52:21 -0300

    Quitado el listado desde MainActivity   - El main contiene un boton donde lleva al listado   - Creado un nuevo activity ListadoPublicaciones donde se mostrara el listado

--- app/src/main/java/com/cabezasfive/truekapp/MainActivity.java
+++ app/src/main/java/com/cabezasfive/truekapp/MainActivity.java
@@ -1,18 +1,10 @@
 package com.cabezasfive.truekapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
-
-import com.cabezasfive.truekapp.entities.Publicacion;
-import com.cabezasfive.truekapp.models.PublicacionViewModel;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
