b8b1c85490affd9e210beb28aa607cc144d3de70
Pablo Clemente <pclementep@gmail.com>
2021-03-23 10:58:55 -0300

    Continuacion de DrawerMenu  - Se agrego un fragment HomeFragment que sera el que carga al iniciar la aplicacion  - Hay un switch en MainActivity para chequear que item se selecciona del menu y de ahi debe cargar el fragment correspondiente

--- app/src/main/java/com/cabezasfive/truekapp/MainActivity.java
+++ app/src/main/java/com/cabezasfive/truekapp/MainActivity.java
@@ -1,28 +1,82 @@
 package com.cabezasfive.truekapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.cabezasfive.truekapp.fragments.HomeFragment;
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // UI
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navView);
+        toolbar = findViewById(R.id.toolbar);
+
+        // Al iniciar se muestra el HomeFragment
+        getSupportFragmentManager().beginTransaction().add(R.id.contenido, new HomeFragment()).commit();
+        setTitle("Home");
+
+        // Setup del toolbar
+        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        selectItemNav(item);
+        return true;
+    }
+
+    private void selectItemNav(MenuItem item) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                ft.replace(R.id.contenido, new HomeFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                break;
+            case R.id.nav_event:
+                break;
+            case R.id.nav_home2:
+                break;
+            case R.id.nav_profile2:
+                break;
         }
+        setTitle(item.getTitle());
+        drawerLayout.closeDrawers();
+    }
 
 
-
-        // Metodo del boton  A Listado
+    // Metodo del boton  A Listado
         public void aListado(View view){
             Intent intent = new Intent(this, ListadoPublicaciones.class);
             startActivity(intent);
         }
+
+
 }
---
+++ app/src/main/java/com/cabezasfive/truekapp/fragments/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.cabezasfive.truekapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cabezasfive.truekapp.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructors
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
