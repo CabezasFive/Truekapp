b9a1edad160e0fb095f2fa6b314132f0a1178d40
Pablo Clemente <pclementep@gmail.com>
2021-03-28 21:18:16 -0300

    Idem anterior se agrego los archivos que no estaban versionados para el funcionamiento google-service.json y Publicacion.java

---
+++ app/src/main/java/com/cabezasfive/truekapp/models/Publicacion.java
@@ -0,0 +1,40 @@
+package com.cabezasfive.truekapp.models;
+
+public class Publicacion {
+
+    private String uid;
+    private String titulo;
+    private String descripcion;
+
+    public Publicacion() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
+}
