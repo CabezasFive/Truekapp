9d766b49a0a0060cbca2a91e480199365abd5d1c
Axel Arres <axelarres1303@yahoo.com>
2021-03-29 15:58:54 -0300

    Mejora Visual y display de puntos en parte inferior de AyudaActivity.

--- app/src/main/java/com/cabezasfive/truekapp/AyudaActivity.java
+++ app/src/main/java/com/cabezasfive/truekapp/AyudaActivity.java
@@ -17,12 +17,15 @@ import com.google.android.material.tabs.TabLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.cabezasfive.truekapp.ui.main.SectionsPagerAdapter;
 
@@ -31,27 +34,61 @@ public class AyudaActivity extends AppCompatActivity implements AyudaDescripcion
         AyudaCategoriasFragment.OnFragmentInteractionListener, AyudaDestacadosFragment.OnFragmentInteractionListener,
         AyudaMasVistosFragment.OnFragmentInteractionListener, AyudaFAQFragment.OnFragmentInteractionListener {
 
+    ViewPager viewPager;
+    private LinearLayout linearPuntos;
+    private TextView[] puntosSlide;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        linearPuntos = findViewById(R.id.idLinearPuntos);
+        agregaIndicadorPuntos(0);
+
+        viewPager.addOnPageChangeListener(viewListener);
     }
+
+    private void agregaIndicadorPuntos(int pos) {
+        puntosSlide =new TextView[7];
+        linearPuntos.removeAllViews();
+
+        for (int i=0; i< puntosSlide.length; i++){
+            puntosSlide[i]=new TextView(this);
+            puntosSlide[i].setText(Html.fromHtml("&#8226;"));
+            puntosSlide[i].setTextSize(35);
+            puntosSlide[i].setTextColor(getResources().getColor(R.color.colorBlancoTransparente));
+            linearPuntos.addView(puntosSlide[i]);
+        }
+
+        if(puntosSlide.length>0){
+            puntosSlide[pos].setTextColor(getResources().getColor(R.color.black));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener=new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int pos) {
+            agregaIndicadorPuntos(pos);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 
     @Override
     public void onFragmentInteraction(Uri uri) {
     }
+
 }
