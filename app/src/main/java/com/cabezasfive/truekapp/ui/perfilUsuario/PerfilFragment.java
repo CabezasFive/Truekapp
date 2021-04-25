dbde9cb2be41b92c4fe4043a84029e46ed2558ef
Pablo Clemente <pclementep@gmail.com>
2021-04-25 19:16:23 -0300

    Creado el layout de perfil de usuario

--- app/src/main/java/com/cabezasfive/truekapp/ui/perfilUsuario/PerfilFragment.java
+++ app/src/main/java/com/cabezasfive/truekapp/ui/perfilUsuario/PerfilFragment.java
@@ -2,7 +2,9 @@ package com.cabezasfive.truekapp.ui.perfilUsuario;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,7 +15,7 @@ import com.cabezasfive.truekapp.R;
 
 public class PerfilFragment extends Fragment {
 
-
+    CardView cardPerfil, cardPublicaciones, cardCreditos, cardSolicitudes;
 
 
     public static PerfilFragment newInstance(String param1, String param2) {
@@ -34,6 +36,39 @@ public class PerfilFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_perfil, container, false);
 
+        cardPerfil = view.findViewById(R.id.cardMisDatos);
+        cardPublicaciones = view.findViewById(R.id.cardMisPublicaciones);
+        cardSolicitudes = view.findViewById(R.id.cardMisOfertas);
+        cardCreditos = view.findViewById(R.id.cardMisCreditos);
+
+
+        cardPublicaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.misPublicacionesFragment);
+            }
+        });
+
+        cardPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aqui va a editar el perfil de usuario
+            }
+        });
+
+        cardSolicitudes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aqui va a ver las solicitudes que se le enviaron
+            }
+        });
+
+        cardCreditos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aqui a la gestion de creditos de usuario
+            }
+        });
 
 
         return view;
